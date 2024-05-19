eb6913670d4d0f37761ca8805a2c4da7a311a043
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2024-05-18 21:55:47 -0500

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,25 +4,14 @@ const PORT = process.env.PORT || 3000;
 const baseURL = `http://localhost:${PORT}`;
 
 const config: PlaywrightTestConfig = {
-    // Timeout per test
     timeout: 10 * 1000,
-
-    fullyParallel: !process.env.CI,
-
-    // If a test fails, retry it additional 2 times
-    // retries: 2,
-
-    // Run your local dev server before starting the tests:
+    use: {
+        baseURL,
+    },
     webServer: {
         command: 'npm run dev',
         port: 3000,
         reuseExistingServer: !process.env.CI,
-    },
-
-    use: {
-        // Use baseURL so to make navigations relative.
-        // More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
-        baseURL,
     },
 };
 
